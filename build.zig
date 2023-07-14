7dc26fbf7d7be37b93d7eb5ea6b946ec00dce044
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-07-14 14:13:04 +0000

    Updated library version.

--- build.zig
+++ build.zig
@@ -2,21 +2,16 @@ pub const zl = @import("./zig_lib/zig_lib.zig");
 
 pub const Node = zl.build.GenericNode(.{});
 
+const build_cmd = .{
+    .kind = .exe,
+    .mode = .ReleaseSmall,
+    .strip = true,
+    .compiler_rt = false,
+};
+
 pub fn buildMain(allocator: *zl.build.Allocator, toplevel: *Node) !void {
-    const all: *Node = try toplevel.addGroup(allocator, "all");
-    const treez: *Node = try all.addBuild(
-        allocator,
-        .{ .kind = .exe, .mode = .ReleaseSmall, .strip = true },
-        "treez",
-        "./src/main.zig",
-    );
-    const zl_treez: *Node = try all.addBuild(
-        allocator,
-        .{ .kind = .exe, .mode = .ReleaseSmall, .strip = true },
-        "zl_treez",
-        "./zig_lib/examples/treez.zig",
-    );
+    const treez: *Node = toplevel.addBuild(allocator, build_cmd, "treez", "./src/main.zig");
+    const zl_treez: *Node = toplevel.addBuild(allocator, build_cmd, "zl_treez", "./zig_lib/examples/treez.zig");
     treez.descr = "List contents of directories in a tree-like format";
     zl_treez.descr = "List contents of directories in a tree-like format, but faster";
-    zl_treez.task.info.build.name = "treez";
 }
